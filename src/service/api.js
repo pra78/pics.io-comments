7c141b95e27a661678eeebbdd6d222800966b1d1
pra78 <66877961+pra78@users.noreply.github.com>
2023-06-19 21:23:02 +0300

    favicon in safari

--- src/service/api.js
+++ src/service/api.js
@@ -1,3 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = "";
+const BASE_URL = "https://dummyjson.com/comments";
+
+axios.defaults.baseURL = BASE_URL;
+
+export const getComments = () => {
+  return axios.get().then((res) => res);
+};
